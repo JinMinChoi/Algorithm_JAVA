0a61f304c673020f50a637fc0fe498aa8d53b2ac
JinMinChoi <jinmin2216@naver.com>
2021-12-02 15:35:25 +0900

    feat: Add Inflearn's question solved

---
+++ src/inflearnLecture/question/Q1.java
@@ -0,0 +1,22 @@
+package inflearnLecture.question;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
